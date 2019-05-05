5e158b87f2453b0f373a3c9d76464fdd7dacd391
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2019-05-06 02:22:39 +0430

    Lite Version

--- android/app/src/main/java/com/textmining/MainApplication.java
+++ android/app/src/main/java/com/textmining/MainApplication.java
@@ -3,10 +3,6 @@ package com.textmining;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,11 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNDeviceInfo(),
-            new RNI18nPackage(),
-            new VectorIconsPackage(),
-            new RNGestureHandlerPackage()
+          new MainReactPackage()
       );
     }
 
